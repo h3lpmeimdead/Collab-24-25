ade42becbb9bb58b0a7396d085dfadc6841be08d
but0512 <trantrunghieu0512@gmail.com>
2025-04-09 11:46:57 +0700

    Changes to level 1

--- Assets/TrungHieu/Script/Bullet.cs
+++ Assets/TrungHieu/Script/Bullet.cs
@@ -3,24 +3,24 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private PlayerShooting shootingScript;
-    public float lifeTime = 5f;
+    public float lifeTime = 3f;
     public float knockbackForce = 10f; // Adjustable knockback force
 
     private void Start()
     {
         shootingScript = FindObjectOfType<PlayerShooting>();
 
-        // Schedule destruction of the bullet after its lifetime
+        //Schedule destruction of the bullet after its lifetime
         Invoke(nameof(ReturnToPoolOrDestroy), lifeTime);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Wall"))
+        if (collision.gameObject.tag == "Wall")
         {
             ReturnToPoolOrDestroy();
         }
-        else if (collision.gameObject.CompareTag("Player"))
+        else if (collision.gameObject.tag == "Player1")
         {
            
             Vector2 knockbackDirection = (collision.transform.position - transform.position).normalized;
@@ -42,10 +42,6 @@ public class Bullet : MonoBehaviour
         if (shootingScript != null)
         {
             shootingScript.ReturnProjectileToPool(gameObject);
-        }
-        else
-        {
-            Destroy(gameObject);
         }
     }
 }
