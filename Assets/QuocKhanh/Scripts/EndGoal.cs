6ab9149ee329202b828aa928d673954dc30a4fb8
Davetheduck03 <156407317+Davetheduck03@users.noreply.github.com>
2025-04-29 23:16:52 +0700

    End goal done

---
+++ Assets/QuocKhanh/Scripts/EndGoal.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGoal : MonoBehaviour
+{
+    [SerializeField] private bool player1Ready = false;
+    [SerializeField] private bool player2Ready = false;
+    [SerializeField] private LayerMask player1Layer;
+    [SerializeField] private LayerMask player2Layer;
+    [SerializeField] private Color bothPlayersColor;
+    [SerializeField] private Color onePlayerColor;
+    [SerializeField] private Color defaultColor;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if ((player1Layer.value & (1 << collision.gameObject.layer)) != 0)
+        {
+            player1Ready = true;
+        }
+
+        if ((player2Layer.value & (1 << collision.gameObject.layer)) != 0)
+        {
+            player2Ready = true;
+        }
+
+        CheckBothPlayers();
+        ChangeColor();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if ((player1Layer.value & (1 << collision.gameObject.layer)) != 0)
+        {
+            player1Ready = false;
+        }
+
+        if ((player2Layer.value & (1 << collision.gameObject.layer)) != 0)
+        {
+            player2Ready = false;
+        }
+        ChangeColor();
+    }
+
+    private void ChangeColor()
+    {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+
+        if (player1Ready && player2Ready)
+        {
+            spriteRenderer.color = bothPlayersColor;
+        }
+        else if (player1Ready || player2Ready)
+        {
+            spriteRenderer.color = onePlayerColor;
+        }
+        else
+        {
+            spriteRenderer.color = defaultColor;
+        }
+    }
+
+    private void CheckBothPlayers()
+    {
+        if (player1Ready && player2Ready)
+        {
+            Debug.Log("Both players reached the goal!");
+        }
+    }
+}
